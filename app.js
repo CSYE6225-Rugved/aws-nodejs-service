da1c2243628f470e5440e38d59ca75542a2dc56b
Rugved-142 <gundawar.r@northeastern.edu>
2025-02-05 01:48:22 -0500

    Added Test cases for API

--- app.js
+++ app.js
@@ -5,21 +5,53 @@ const sequelize = require("./config/config");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
-
-// Start the server
-const server = app.listen(PORT, async () => {
-    try {
-        await sequelize.authenticate();
-        console.log(`Server is running on http://localhost:${PORT}`);
-    } catch (error) {
-        console.error("Database connection failed:", error);
-        process.exit(1);
-    }
-});
+const ENV = process.env.NODE_ENV || "development";
 
 // Middleware
-app.use(express.json());
+app.use(express.json({strict : true}));
+app.use((req,res,next) => {
+    if(Object.keys(req.query).length > 0){
+        console.error("Query Parameters not allowed");
+        res.set("Cache-Control", "no-cache");
+        return res.status(400).send();
+    }
+    next();
+});
+app.use((err,req,res,next) => {
+    if(err instanceof SyntaxError && err.status === 400 && "body" in err){
+        console.error("Invalid JSON payload received");
+        res.set("Cache-Control", "no-cache");
+        return res.status(400).send();
+    }
+    next();
+});
 
 // Routes
 app.use("/", routes);
+app.use((req,res) => {
+    console.error("Invalid API endpoint");
+    res.status(404).send();
+});
 
+// Bootstrapping Database
+(async () => {
+    try {
+        await sequelize.sync();
+        console.log("HealthCheck table Synced Successfully");
+    } catch (error) {
+        console.error("Error syncing the HealthCheck table:", error);
+    }
+})();
+
+if (ENV !== "test") {
+    app.listen(PORT, async () => {
+        try {
+            await sequelize.authenticate();
+            console.log(`Server is running on http://localhost:${PORT}`);
+        } catch (error) {
+            console.error("Database connection failed:", error);
+            process.exit(1);
+        }
+    });
+}
+module.exports = app;
--- models/model.js
+++ models/model.js
@@ -1,27 +1,32 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../config/config");
-
-// Define HealthCheck Model
-const HealthCheck = sequelize.define("HealthCheck", {
-    id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
+const HealthCheck = sequelize.define(
+    "HealthCheck",
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        datetime: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW,
+        }
     },
-    datetime: {
-        type: DataTypes.DATE,
-        defaultValue: DataTypes.NOW,
-    },
-});
-
-// Bootstrapping Database
-(async () => {
-    try {
-        await sequelize.sync();
-        console.log("HealthCheck table Synced Successfully");
-    } catch (error) {
-        console.error("Error syncing the HealthCheck table:", error);
+    {
+        timestamps: false 
     }
-})();
+);
+
+
+// // Bootstrapping Database
+// (async () => {
+//     try {
+//         await sequelize.sync();
+//         console.log("HealthCheck table Synced Successfully");
+//     } catch (error) {
+//         console.error("Error syncing the HealthCheck table:", error);
+//     }
+// })();
 
 module.exports = HealthCheck;
