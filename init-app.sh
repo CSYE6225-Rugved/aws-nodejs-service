7a4935468b00dacd0cd57d440400de5e62176b79
Rugved-142 <gundawar.r@northeastern.edu>
2025-03-10 12:35:43 -0400

    resolved errors

--- init-app.sh
+++ init-app.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+sudo apt-get update
+sudo apt-get install -y unzip
+sudo apt-get install -y nodejs
+sudo apt-get install -y npm
 echo "Setting up the application in the custom image..."
 echo "Creating group and user..."
 
@@ -7,12 +11,11 @@ sudo groupadd -r csye6225
 sudo useradd -r -s /usr/sbin/nologin -g csye6225 csye6225
 
 echo "Setting up application directory..."
+sudo mkdir -p /opt/webapp
 sudo cp /tmp/webapp.zip /opt/
 sudo unzip /opt/webapp.zip -d /opt/webapp
-cd /opt/webapp
+cd /opt/webapp 
 sudo chown -R csye6225:csye6225 /opt/webapp
-sudo npm install
-sudo chown csye6225:csye6225 node_modules
 
 echo "Configuring systemd service..."
 sudo cp tmp/webapp.service /etc/systemd/system/
